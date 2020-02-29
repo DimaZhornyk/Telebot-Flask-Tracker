ffa854733ade103c372518a8f944dcf24d54ace1
Дмитро Жорник <dmytro.zhornyk@ukma.edu.ua>
2020-02-29 14:57:34 +0200

    Updated exceptions, fixed some bugs

--- resources/table_names.py
+++ resources/table_names.py
@@ -6,7 +6,11 @@ from flask_restful import Resource
 class TableNames(Resource):
     @jwt_required
     def post(self):
-        return db.list_collection_names()
+        all_names = db.list_collection_names()
+        all_names.remove('sequences')
+        all_names.remove('Metadata')
+        all_names.remove('Users')
+        return all_names
 
 
 class RequiredFields(Resource):
--- resources/tables.py
+++ resources/tables.py
@@ -12,7 +12,7 @@ class Tables(Resource):
         data = parser.parse_args()
         if db[data['Name']]:  # if table exists
             table = db[data['Name']].find()  # taking the table
-            metadata = db['Metadata'].find_one({"Name": data['Name']});
+            metadata = db['Metadata'].find_one({"Name": data['Name']})
             allowed_keys = metadata["keys"]  # taking the keys to return from table
             to_return = []
             for el in table:  # checking each table line and taking the needed elements
@@ -26,25 +26,27 @@ class Tables(Resource):
                 except:
                     pass
                 to_return.append(res)
-                metadata.pop('_id', None)
+            metadata.pop('_id', None)
             return {"table": to_return, "metadata": metadata}
         return {"message": "Table not found"}
 
     # to edit an existing table
     @jwt_required
     def patch(self):
-        data = request.get_json(force=True)
+        try:
+            data = request.get_json(force=True)
+        except:
+            return {}, 400
         try:
             allowed_keys = db['Metadata'].find_one({"Name": data['tableName']})["keys"]
         except:
-            return {"message": "An error occurred"}
+            return {}, 400
         table_name = data['tableName']
         rows_to_edit = data['rowsToEdit']
         rows_to_add = data['rowsToAdd']
         rows_to_delete = data['rowsToDelete']
         for row in rows_to_edit:
             new_values = {}
-            print(row);
             for key in row.keys():
                 if key in allowed_keys:
                     new_values[key] = row[key]
@@ -63,8 +65,7 @@ class Tables(Resource):
             elif db['Metadata'].find_one({"Name": table_name})["containsGeo"]:
                 query = {'ID': int(index)}
             else:
-                return {"message": "Error occurred while deleting"}
-            print(query);
+                return {}, 400
             db[table_name].delete_one(query)
 
         if db['Metadata'].find_one({"Name": table_name})["containsWorkers"]:
@@ -72,19 +73,19 @@ class Tables(Resource):
         elif db['Metadata'].find_one({"Name": table_name})["containsGeo"]:
             workers = False
         else:
-            return {"message": "Error occurred while adding"}
+            return {}, 400
         for row in rows_to_add:
             new_value = {}
-            if not workers:
-                new_value['ID'] = get_sequence('loc')
             for key in row.keys():
                 if key in allowed_keys:
                     new_value[key] = row[key]
+            if not workers:
+                new_value['ID'] = get_sequence('loc')
             if workers is False and db[table_name].find_one({"Name": row["Name"]}) is None:
                 db[table_name].insert_one(new_value)
             elif workers and db[table_name].find_one({"Telegram": row["Telegram"]}) is None:
                 db[table_name].insert_one(new_value)
-        return {"message": "Successfully done"}
+        return {"message": "Successfully done"}, 200
 
     # to create a new table
     @jwt_required
@@ -96,11 +97,12 @@ class Tables(Resource):
         keys = eval(data["keys"])
         for key in db["Metadata"].find_one({"Name": "Required fields"})["keys"]:
             if key not in keys:
-                return {"message": "Not all required keys specified"}
+                return {"message": "Not all required keys specified"}, 400
         if data["Name"] in db.list_collection_names():
-            return {"message": "Table with this name already exists"}
+            return {"message": "Table with this name already exists"}, 400
         collection = db[data["Name"]]
         collection.insert_one({"Initial": "Initial"})
+        collection.delete_one({"Initial": "Initial"})
         db["Metadata"].insert_one({data['Name']: {
             "keys": keys,
             "containsWorkers": True,
@@ -118,7 +120,19 @@ class Tables(Resource):
             db[table_name].drop()
             return {"message": "Successfully deleted"}
         except:
-            return {"message": "An error occurred during deletion"}
+            return {}, 400
+
+
+class Worker(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("Telegram", type=str, required=True, help='Name cannot be left blank')
+        data = parser.parse_args()
+        try:
+            worker = db['History'].find_one({"Telegram": data['Telegram']}).pop("_id")
+        except:
+            return {}, 400
+        return
 
 
 def get_sequence(name):
