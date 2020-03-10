6720cb54b0adcce748501a22ad6e816e912b8b62
BogdanVoylov <voylovbogdan@gmail.com>
2020-03-10 16:44:18 +0200

    attempt to add static

--- app.py
+++ app.py
@@ -2,26 +2,26 @@ from datetime import timedelta
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from flask import Flask, redirect, send_from_directory
+from whitenoise import WhiteNoise
+
 from resources.user import UserLogin
 from resources.table_names import TableNames, RequiredFields
 from resources.tables import Tables, Worker
 from flask_cors import CORS
 
-app = Flask(__name__,
-            static_url_path='/',
-            static_folder=".\\static",
-            )
+app = Flask(__name__,)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
 api = Api(app)
 CORS(app)
 app.secret_key = 'sssss'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=7200)
 jwt = JWTManager(app)
 
-@app.route('/')
-def root():
-    #return send_from_directory('static', 'index.html')
-    return app.send_static_file('index.html')
-
+# @app.route('/')
+# def root():
+#     return send_from_directory('static', 'index.html')
+#     # return app.send_static_file('index.html')
+#
 
 api.add_resource(TableNames, '/table_names')
 api.add_resource(Tables, '/tables')
